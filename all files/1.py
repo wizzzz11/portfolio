1ad1cd1a73cc837eafba38756461e666c3905a5c
wizzzz11 <2006sourabratasaha@gmail.com>
2026-04-04 02:18:03 +0530

    removed all files folder

--- all files/1.py
+++
@@ -1,5 +0,0 @@
-print("subrata saha")
-a=66
-b=99
-c=a+b
-print(c)
--- all files/assets/js/script.js
+++
@@ -1,4 +0,0 @@
-// Replace with your actual profile links
-document.getElementById("githubLink").href = "https://github.com/wizzzz11";
-document.getElementById("linkedinLink").href = "https://www.linkedin.com/in/sourabrata-saha-207a51309";
-document.getElementById("emailLink").href = "mailto:2006sourabratasaha@gmail.com";
