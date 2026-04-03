e85296e3fa702d8e17ffa205ee63410aeeae0862
wizzzz11 <2006sourabratasaha@gmail.com>
2026-04-04 00:26:03 +0530

    added mobile navbar

--- portfolio/assets/js/script.js
+++ portfolio/assets/js/script.js
@@ -2,3 +2,9 @@
 document.getElementById("githubLink").href = "https://github.com/wizzzz11";
 document.getElementById("linkedinLink").href = "https://www.linkedin.com/in/sourabrata-saha-207a51309";
 document.getElementById("emailLink").href = "mailto:2006sourabratasaha@gmail.com";
+const menuToggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav");
+
+menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
